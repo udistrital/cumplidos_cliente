4e4601ec44e3032028047e7604d06c3b044c0c1c
Andrea <anlramirezs11@gmail.com>
2019-09-10 11:48:59 -0500

    Se configura el ambiente de desarrollo y se elimina los ambientes de Preproduccion y test. Issue #10

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -8,237 +8,92 @@
  * Constant in the contractualClienteApp.
  */
 var conf_cloud = {
-    WSO2_SERVICE: "http://jbpm.udistritaloas.edu.co:8280/services",
-    ADMINISTRATIVA_MID_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/api/administrativa_mid_api/v1/",
-    ADMINISTRATIVA_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/api/administrativa_crud_api/v1/",
-    ADMINISTRATIVA_PRUEBAS_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/api/administrativa_amazon_api/v1/",
-    CONFIGURACION_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/api/configuracion_crud_api/v1/",
-    CORE_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/api/core_api/v1/",
-    FINANCIERA_MID_SERVICE: "https://tuleap.udistrital.edu.co/go_api/financiera_mid_api/v1/",
-    FINANCIERA_SERVICE: "https://tuleap.udistrital.edu.co/go_api/financiera_api/v1/",
-    MODELS_SERVICE: "scripts/models/",
-    NOTIFICACION_WS: "ws://10.20.2.134:8080/ws/join",
-    OIKOS_SERVICE: "https://tuleap.udistrital.edu.co/go_api/oikos_api/v1/",
-    CONTRATO_SERVICE: "https://autenticacion.udistrital.edu.co:8244/administrativa_jbpm/v1/",
-    NUXEO_SERVICE: "https://documental.portaloas.udistrital.edu.co/nuxeo/",
-    TOKEN: {
-      AUTORIZATION_URL: "https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize",
-        URL_USER_INFO: "https://autenticacion.portaloas.udistrital.edu.co/oauth2/userinfo",
-        CLIENTE_ID: "pszmROXqfec4pTShgF_fn2DAAX0a",
-        REDIRECT_URL: "http://localhost:9000/",
-        RESPONSE_TYPE: "id_token token",
-        SCOPE: "openid email documento",
-        BUTTON_CLASS: "btn btn-warning btn-sm",
-        SIGN_OUT_URL: "https://autenticacion.portaloas.udistrital.edu.co/oidc/logout",
-        SIGN_OUT_REDIRECT_URL: "http://localhost:9000/",
-        SIGN_OUT_APPEND_TOKEN: "true",
-    },
-    MENU_APP: [{
-      id: "kronos",
-      title: "KRONOS",
-      url: "http://10.20.0.254/kronos"
-    }, {
-      id: "agora",
-      title: "AGORA",
-      url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/agora"
-    },
-    {
-      id: "argo",
-      title: "ARGO",
-      url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/argo"
-    },
-    {
-      id: "arka",
-      title: "ARKA",
-      url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/arka"
-    },
-    {
-      id: "temis",
-      title: "TEMIS",
-      url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/gefad"
-    },
-    {
-      id: "polux",
-      title: "POLUX",
-      url: "http://10.20.0.254/polux"
-    },
-    {
-      id: "jano",
-      title: "JANO",
-      url: "http://10.20.0.254/kronos"
-    },
-    {
-      id: "kyron",
-      title: "KYRON",
-      url: "http://10.20.0.254/kronos"
-    },
-    {
-      id: "sga",
-      title: "SGA",
-      url: "http://10.20.0.254/kronos"
-    }
-  ]
-};
-var conf_preproduccion = {
-    WSO2_SERVICE: "http://jbpm.udistritaloas.edu.co:8280/services",
-    ADMINISTRATIVA_MID_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_mid_api/v1/",
-    ADMINISTRATIVA_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_crud_api/v1/",
-    ADMINISTRATIVA_PRUEBAS_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_amazon_api/v1/",
-    CONFIGURACION_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/",
-    CORE_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/core_api/v1/",
-    FINANCIERA_MID_SERVICE: "https://tuleap.udistrital.edu.co/go_api/financiera_mid_api/v1/",
-    FINANCIERA_SERVICE: "https://tuleap.udistrital.edu.co/go_api/financiera_api/v1/",
-    MODELS_SERVICE: "scripts/models/",
-    NOTIFICACION_WS: "ws://10.20.2.134:8080/ws/join",
-    OIKOS_SERVICE: "https://tuleap.udistrital.edu.co/go_api/oikos_api/v1/",
-    CONTRATO_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_jbpm/v1/",
-    NUXEO_SERVICE: "https://documental.portaloas.udistrital.edu.co/nuxeo/",
-    TOKEN: {
-      AUTORIZATION_URL: "https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize",
-        URL_USER_INFO: "https://autenticacion.portaloas.udistrital.edu.co/oauth2/userinfo",
-        CLIENTE_ID: "f0zmi1cih0yKrLwEsoJdqI1vASQa",
-        REDIRECT_URL: "https://pruebasadministrativa.portaloas.udistrital.edu.co",
-        RESPONSE_TYPE: "id_token token",
-        SCOPE: "openid email documento",
-        BUTTON_CLASS: "btn btn-warning btn-sm",
-        SIGN_OUT_URL: "https://autenticacion.portaloas.udistrital.edu.co/oidc/logout",
-        SIGN_OUT_REDIRECT_URL: "https://pruebasadministrativa.portaloas.udistrital.edu.co",
-        SIGN_OUT_APPEND_TOKEN: "true",
-    },
-    MENU_APP: [{
-      id: "kronos",
-      title: "KRONOS",
-      url: "http://10.20.0.254/kronos"
-    }, {
-      id: "agora",
-      title: "AGORA",
-      url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/agora"
-    },
-    {
-      id: "argo",
-      title: "ARGO",
-      url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/argo"
-    },
-    {
-      id: "arka",
-      title: "ARKA",
-      url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/arka"
-    },
-    {
-      id: "temis",
-      title: "TEMIS",
-      url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/gefad"
-    },
-    {
-      id: "polux",
-      title: "POLUX",
-      url: "http://10.20.0.254/polux"
-    },
-    {
-      id: "jano",
-      title: "JANO",
-      url: "http://10.20.0.254/kronos"
-    },
-    {
-      id: "kyron",
-      title: "KYRON",
-      url: "http://10.20.0.254/kronos"
-    },
-    {
-      id: "sga",
-      title: "SGA",
-      url: "http://10.20.0.254/kronos"
-    }
-  ]
-};
-
-var conf_pruebas = {
-    WSO2_SERVICE: "http://jbpm.udistritaloas.edu.co:8280/services",
-  ADMINISTRATIVA_MID_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_mid_api/v1/",
-  ADMINISTRATIVA_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_crud_api/v1/",
-  ADMINISTRATIVA_PRUEBAS_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_amazon_api/v1/",
-  CONFIGURACION_SERVICE: "http://localhost:8088/v1/",
-  CORE_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/core_api/v1/",
+  WSO2_SERVICE: "http://jbpm.udistritaloas.edu.co:8280/services",
+  ADMINISTRATIVA_MID_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/api/administrativa_mid_api/v1/",
+  ADMINISTRATIVA_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/api/administrativa_crud_api/v1/",
+  ADMINISTRATIVA_PRUEBAS_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/api/administrativa_amazon_api/v1/",
+  CONFIGURACION_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/api/configuracion_crud_api/v1/",
+  CORE_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/api/core_api/v1/",
   FINANCIERA_MID_SERVICE: "https://tuleap.udistrital.edu.co/go_api/financiera_mid_api/v1/",
   FINANCIERA_SERVICE: "https://tuleap.udistrital.edu.co/go_api/financiera_api/v1/",
-    MODELS_SERVICE: "scripts/models/",
-    NOTIFICACION_WS: "ws://localhost:8080/ws/join",
-  OIKOS_SERVICE: "https://tuleap.udistrital.edu.co/go_api/oikos_api/v1/",
-  CONTRATO_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_jbpm/v1/",
-  NUXEO_SERVICE: "https://documental.portaloas.udistrital.edu.co/nuxeo/",
-    TOKEN: {
-      AUTORIZATION_URL: "https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize",
-      URL_USER_INFO: "https://autenticacion.portaloas.udistrital.edu.co/oauth2/userinfo",
-      CLIENTE_ID: "Xx5xAFRoKKFv0Clf3igvNsLIz0Aa",
-      REDIRECT_URL: "http://localhost:9000/",
-      RESPONSE_TYPE: "id_token token",
-      SCOPE: "openid email documento",
-      BUTTON_CLASS: "btn btn-warning btn-sm",
-      SIGN_OUT_URL: "https://autenticacion.udistrital.edu.co/oidc/logout",
-      SIGN_OUT_REDIRECT_URL: "http://localhost:9000/",
-      SIGN_OUT_APPEND_TOKEN: "true",
-    },
-    MENU_APP: [{
-      id: "kronos",
-      title: "KRONOS",
-      url: "http://10.20.0.254/kronos"
-    }, {
-      id: "agora",
-      title: "AGORA",
-      url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/agora"
-    },
-    {
-      id: "argo",
-      title: "ARGO",
-      url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/argo"
-    },
-    {
-      id: "arka",
-      title: "ARKA",
-      url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/arka"
-    },
-    {
-      id: "temis",
-      title: "TEMIS",
-      url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/gefad"
-    },
-    {
-      id: "polux",
-      title: "POLUX",
-      url: "http://10.20.0.254/polux"
-    },
-    {
-      id: "jano",
-      title: "JANO",
-      url: "http://10.20.0.254/kronos"
-    },
-    {
-      id: "kyron",
-      title: "KYRON",
-      url: "http://10.20.0.254/kronos"
-    },
-    {
-      id: "sga",
-      title: "SGA",
-      url: "http://10.20.0.254/kronos"
-  }]
-};
-
-var conf_local = {
-  WSO2_SERVICE: "http://jbpm.udistritaloas.edu.co:8280/services",
-  ACADEMICA_WSO_SERVICE: "https://jbpm.udistritaloas.edu.co:8243/services/academicaProxy/",
-  ADMINISTRATIVA_MID_SERVICE: "http://10.20.0.254/administrativa_mid_api/v1/",
-  ADMINISTRATIVA_SERVICE: "http://10.20.0.254/administrativa_api/v1/",
-  ADMINISTRATIVA_PRUEBAS_SERVICE:"http://10.20.0.254/administrativa_amazon_api/v1/",
-  CONFIGURACION_SERVICE: "http://10.20.0.254/configuracion_api/v1/",
-  CORE_SERVICE: "http://10.20.0.254/core_api/v1/",
-  FINANCIERA_MID_SERVICE: "http://10.20.0.254/financiera_mid_api/v1/",
-  FINANCIERA_SERVICE: "http://10.20.0.254/financiera_api/v1/",
   MODELS_SERVICE: "scripts/models/",
   NOTIFICACION_WS: "ws://10.20.2.134:8080/ws/join",
-  OIKOS_SERVICE: "http://10.20.0.254/oikos_api/v1/",
-  CONTRATO_SERVICE: "https://autenticacion.udistrital.edu.co/apioas/administrativa_jbpm/v1/",
-  NUXEO_SERVICE: "https://documental.udistrital.edu.co/nuxeo/api/v1/",
+  OIKOS_SERVICE: "https://tuleap.udistrital.edu.co/go_api/oikos_api/v1/",
+  CONTRATO_SERVICE: "https://autenticacion.udistrital.edu.co:8244/administrativa_jbpm/v1/",
+  NUXEO_SERVICE: "https://documental.portaloas.udistrital.edu.co/nuxeo/",
+  TOKEN: {
+    AUTORIZATION_URL: "https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize",
+    URL_USER_INFO: "https://autenticacion.portaloas.udistrital.edu.co/oauth2/userinfo",
+    CLIENTE_ID: "pszmROXqfec4pTShgF_fn2DAAX0a",
+    REDIRECT_URL: "http://localhost:9000/",
+    RESPONSE_TYPE: "id_token token",
+    SCOPE: "openid email documento",
+    BUTTON_CLASS: "btn btn-warning btn-sm",
+    SIGN_OUT_URL: "https://autenticacion.portaloas.udistrital.edu.co/oidc/logout",
+    SIGN_OUT_REDIRECT_URL: "http://localhost:9000/",
+    SIGN_OUT_APPEND_TOKEN: "true",
+  },
+  MENU_APP: [{
+    id: "kronos",
+    title: "KRONOS",
+    url: "http://10.20.0.254/kronos"
+  }, {
+    id: "agora",
+    title: "AGORA",
+    url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/agora"
+  },
+  {
+    id: "argo",
+    title: "ARGO",
+    url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/argo"
+  },
+  {
+    id: "arka",
+    title: "ARKA",
+    url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/arka"
+  },
+  {
+    id: "temis",
+    title: "TEMIS",
+    url: "https://pruebasfuncionarios.intranetoas.udistrital.edu.co/gefad"
+  },
+  {
+    id: "polux",
+    title: "POLUX",
+    url: "http://10.20.0.254/polux"
+  },
+  {
+    id: "jano",
+    title: "JANO",
+    url: "http://10.20.0.254/kronos"
+  },
+  {
+    id: "kyron",
+    title: "KYRON",
+    url: "http://10.20.0.254/kronos"
+  },
+  {
+    id: "sga",
+    title: "SGA",
+    url: "http://10.20.0.254/kronos"
+  }
+  ]
+};
+
+var conf_desarrollo = {
+  WSO2_SERVICE: "",
+  ADMINISTRATIVA_MID_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_mid_api/v1",
+  ADMINISTRATIVA_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_crud_api/v1",
+  ADMINISTRATIVA_PRUEBAS_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_amazon_api/v1",
+  CONFIGURACION_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1",
+  CORE_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/core_api/v1",
+  FINANCIERA_MID_SERVICE: "",
+  FINANCIERA_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/financiera_crud_api/v1",
+  MODELS_SERVICE: "scripts/models/",
+  NOTIFICACION_WS: "ws://pruebasapi.intranetoas.udistrital.edu.co:8116/ws/join",
+  OIKOS_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v1",
+  CONTRATO_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_jbpm/v1",
+  NUXEO_SERVICE: "https://documental.udistrital.edu.co/nuxeo/",
   TOKEN: {
     AUTORIZATION_URL: "https://autenticacion.udistrital.edu.co/oauth2/authorize",
     URL_USER_INFO: "https://autenticacion.udistrital.edu.co/oauth2/userinfo",
@@ -291,10 +146,10 @@ var conf_local = {
     title: "SGA",
     url: "http://10.20.0.254/kronos"
   }
-]
+  ]
 };
 
 angular.module('contractualClienteApp')
   .constant('CONF', {
-      GENERAL: conf_local
+    GENERAL: conf_desarrollo
   });
