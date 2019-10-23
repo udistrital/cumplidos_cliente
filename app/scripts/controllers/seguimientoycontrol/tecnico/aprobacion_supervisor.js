8d2d11cce20508cbb1ba3eb6b948ff9b574a8f04
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-10-23 00:51:12 -0500

    parece que se corrigio el error al aprobar los cumplidos como supervisor #17

--- app/scripts/controllers/seguimientoycontrol/tecnico/aprobacion_supervisor.js
+++ app/scripts/controllers/seguimientoycontrol/tecnico/aprobacion_supervisor.js
@@ -253,24 +253,34 @@ angular.module('contractualClienteApp')
           administrativaRequest.put('pago_mensual', self.aux_pago_mensual.Id, self.aux_pago_mensual)
           .then(function (response) {
             swal(
+              'Visto bueno ',
+              'Tiene la validación del supervisor del contrato',
+              'success'
+            )
+            self.obtener_contratistas_supervisor();
+            self.gridApi.core.refresh();
+            // swal(
+            //   'Error',
+            //   'No se ha podido registrar la validación del supervisor',
+            //   'error'
+            // );
+
+
+//Termina promesa then
+         })
+          .catch(function(response) { //Manejo de error
+            swal(
               'Error',
               'No se ha podido registrar la validación del supervisor',
               'error'
             );
-
-
-
-         })//Termina promesa then
-
-         //Manejo de error
-         .catch(function(response) {
-             swal(
-               'Visto bueno ',
-               'Tiene la validación del supervisor del contrato',
-               'success'
-             )
-             self.obtener_contratistas_supervisor();
-             self.gridApi.core.refresh();
+            //  swal(
+            //    'Visto bueno ',
+            //    'Tiene la validación del supervisor del contrato',
+            //    'success'
+            //  )
+            //  self.obtener_contratistas_supervisor();
+            //  self.gridApi.core.refresh();
            });
 
         })
@@ -470,7 +480,21 @@ angular.module('contractualClienteApp')
                 var date = new Date()
                 var dia = moment(date).format('D');
                 var mes = moment(date).format('M');
+                var mes_ss = self.mes.Id-1;
                 var anio = moment(date).format('YYYY');
+                
+                var mes_ss = 0;
+                var anio_ss = 0;
+
+                if (self.mes.Id =='01'){
+                    mes_ss = 12;
+                    anio_ss = self.anio-1;
+                }
+                else{
+                    mes_ss = self.mes.Id-1;
+                    anio_ss = self.anio;
+                }  
+
                 var contenidoInv = [];
                 var contenidoFun = [];
                 
@@ -502,12 +526,10 @@ angular.module('contractualClienteApp')
                    }
                 });
                 
-                
-
                 if(inversion.length>0 ){
                    contenidoInv.push( {text:'EL JEFE DE LA DEPENDENCIA ' +  self.dependencia.nombre  + ' DE LA UNIVERSIDAD DISTRITAL FRANCISCO JOSÉ DE CALDAS', bold: true,  alignment: 'center', style:'top_space'}, '\n\n\n\n');
                    contenidoInv.push({text:'CERTIFICA QUE: ', bold: true,  alignment: 'center', style:'top_space'}, '\n\n\n\n');
-                   contenidoInv.push({text:'Los contratos de prestación de servicios bajo esta supervisión listados a continuación cumplieron a satisfacción con el objeto establecido en el contrato y con el pago reglamentario de los aportes al sistema de seguridad social del Mes de '  +self.mes.Nombre+ ' de ' +self.anio+ '.', style:'general_font'}, '\n\n')
+                   contenidoInv.push({text:'Los contratos de prestación de servicios bajo esta supervisión listados a continuación cumplieron a satisfacción con el objeto establecido en el contrato en el Mes de ' + self.mes.Nombre + ' de ' + self.anio+ ' y con el pago reglamentario de los aportes al sistema de seguridad social del Mes de '  +self.meses[mes_ss-1].Nombre+ ' de ' +anio_ss+ '.', style:'general_font'}, '\n\n')
                    angular.forEach(inversion, function(valueInv) {
                    tablaInv.table.body.push([ valueInv.NumDocumento , valueInv.Nombre, valueInv.NumeroContrato , valueInv.Vigencia, valueInv.Rubro]);
                    });
@@ -521,7 +543,7 @@ angular.module('contractualClienteApp')
                 if(funcionamiento.length>0){
                    contenidoFun.push( {text:'EL JEFE DE LA DEPENDENCIA ' +  self.dependencia.nombre  + ' DE LA UNIVERSIDAD DISTRITAL FRANCISCO JOSÉ DE CALDAS', bold: true,  alignment: 'center', style:'top_space'}, '\n\n\n\n');
                    contenidoFun.push({text:'CERTIFICA QUE: ', bold: true,  alignment: 'center', style:'top_space'}, '\n\n\n\n');
-                   contenidoFun.push({text:'Los contratos de prestación de servicios bajo esta supervisión listados a continuación cumplieron a satisfacción con el objeto establecido en el contrato y con el pago reglamentario de los aportes al sistema de seguridad social del Mes de '  +self.mes.Nombre+ ' de ' +self.anio+ '.', style:'general_font'}, '\n\n')
+                   contenidoFun.push({text:'Los contratos de prestación de servicios bajo esta supervisión listados a continuación cumplieron a satisfacción con el objeto establecido en el contrato en el Mes de ' + self.mes.Nombre + ' de ' + self.anio+ ' y con el pago reglamentario de los aportes al sistema de seguridad social del Mes de '  +self.meses[mes_ss-1].Nombre+ ' de ' +anio_ss+ '.', style:'general_font'}, '\n\n')
                    angular.forEach(funcionamiento, function(valueFun) {
                    tablaFun.table.body.push([ valueFun.NumDocumento , valueFun.Nombre, valueFun.NumeroContrato , valueFun.Vigencia, valueFun.Rubro]);
                    });
