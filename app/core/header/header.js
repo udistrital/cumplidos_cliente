9c28dc858574771660b2d6f5cec70f9e6e1cf1ae
Fabián Sánchez <fadarsaleeing@gmail.com>
2019-11-13 16:00:20 -0500

    @FredyRodriguezR y @jdc996 integran los siguientes botones, menu de aplicaciones y cerrar sesion y se ajustan varios estilos udistrital/catalogo_cliente#11

--- app/core/header/header.js
+++ app/core/header/header.js
@@ -9,27 +9,53 @@
 angular.module('core')
     .controller('headerCtrl', 
     function (token_service, configuracionRequest, notificacion, $translate, $route, $mdSidenav, $scope) {
-        $scope.hola = "cordial saludo";
+
+        
+        $scope.token_service=token_service;
+
+        if(token_service.live_token()){
+            $scope.token=token_service.getPayload();
+
+        }
+        $scope.logout= function(){
+            token_service.logout();
+        }
+
+        $scope.toogleCerrarSesion = function(){
+            var buttonCerrarSesion = document.getElementById('header-button-cerrarsesion-container');
+            if(buttonCerrarSesion.style.display === "none" || buttonCerrarSesion.style.display === ""){
+                buttonCerrarSesion.style.display = "block";
+            }else{
+                buttonCerrarSesion.style.display = "none";
+            }
+            
+        }
+        $scope.toogleAplicaciones = function() {
+            var menuAplicaciones = document.getElementById('menu-aplicaciones');
+
+            console.info(menuAplicaciones)
+            if(menuAplicaciones.className.includes("menu_is_active")){
+                menuAplicaciones.classList.remove("menu_is_active")
+            }else{
+                menuAplicaciones.classList.add("menu_is_active")
+            }
+        }
         $scope.sidebarEvent= function(){
             console.log("data");
             var sidebarDiv = document.getElementById('menu-sidebar');
             var sidebarContainer = document.getElementById('menu-sidebar-container');
             var containerDiv= document.getElementById('container-div');   
-            var containerFooter= document.getElementById('footer-id');   
             var containerBody= document.getElementById('container-body-id');   
             var containerLogoCollapsed= document.getElementById('argo'); 
             var containerLogo= document.getElementById('argo-info'); 
             console.log("container logo :"+containerLogo)
             if (sidebarDiv.className.includes("sidebar_off")){
-                console.log("entro a funcion")
                 sidebarContainer.classList.add('main-container-sidebar')
                 sidebarContainer.classList.remove('main-container-sidebar-off')
                 sidebarDiv.classList.add('sidebar_is_active')
                 sidebarDiv.classList.remove('sidebar_off')
                 containerBody.classList.add('container-body-off')
                 containerBody.classList.remove('container-body')
-                containerFooter.classList.add('container-footer-off')
-                containerFooter.classList.remove('container-footer')
                 containerLogo.style.display = "inline-block";
                 containerLogoCollapsed.style.display = "none";
                 //*********************/
@@ -42,8 +68,6 @@ angular.module('core')
                 sidebarContainer.classList.remove('main-container-sidebar')
                 sidebarDiv.classList.add('sidebar_off')
                 sidebarDiv.classList.remove('sidebar_is_active')
-                containerFooter.classList.add('container-footer')
-                containerFooter.classList.remove('container-footer-off')
                 //*****************/
                 containerLogo.classList.remove('header-logo')
                 containerLogo.classList.add('header-logo-collapsed')
---
+++ app/core/menu-aplicaciones/menu-aplicaciones.js
@@ -0,0 +1,12 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name core.controller:menuaplicacionesCtrl
+ * @description
+ * # menuaplicacionesCtrl
+ * Controller of the core
+ */
+angular.module('core')
+    .controller('menuaplicacionesCtrl', 
+    function (token_service, configuracionRequest, notificacion, $translate, $route, $mdSidenav, $scope) {
+    });
