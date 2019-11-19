5bc5d73415100576fc9274432eaa190d5a0e2d16
Fabián Sánchez <fadarsaleeing@gmail.com>
2019-11-19 16:29:15 -0500

    @FredyRodriguezR implementa la logica para ocultar y mostrar el menu lateral con iconos udistrital/catalogo_cliente#11

--- app/core/header/header.js
+++ app/core/header/header.js
@@ -1,3 +1,5 @@
+
+
 'use strict';
 /**
  * @ngdoc function
@@ -6,91 +8,109 @@
  * # headerCtrl
  * Controller of the core
  */
+
 angular.module('core')
-    .controller('headerCtrl', 
-    function (token_service, CONF, configuracionRequest, notificacion, $translate, $route, $mdSidenav, $scope) {
+    .controller('headerCtrl',
+        function (token_service, CONF, configuracionRequest, notificacion, $translate, $route, $mdSidenav, $scope) {
 
-        $scope.token_service=token_service;
-        $scope.tema = (CONF.CATEGORIA.toLowerCase()).trim() ;
-        $scope.paleta = CONF.THEMES;
+            $scope.token_service = token_service;
+            $scope.tema = (CONF.CATEGORIA.toLowerCase()).trim();
+            $scope.paleta = CONF.THEMES;
 
-        $scope.app = (CONF.APP.toLowerCase()).trim()+ "-isotipo";
-        $scope.app_large = (CONF.APP.toLowerCase()).trim() + "-header";
-
-        if(token_service.live_token()){
-            $scope.isLogin=true
-
-            $scope.token=token_service.getPayload();
-        }
-        $scope.logout= function(){
-            token_service.logout();
-        }
-
-        $scope.toogleCerrarSesion = function(){
-            var buttonCerrarSesion = document.getElementById('header-button-cerrarsesion-container');
-            if(buttonCerrarSesion.style.display === "none" || buttonCerrarSesion.style.display === ""){
-                buttonCerrarSesion.style.display = "block";
-            }else{
-                buttonCerrarSesion.style.display = "none";
-            }
+            $scope.app = (CONF.APP.toLowerCase()).trim() + "-isotipo";
             
-        }
-        $scope.toogleAplicaciones = function() {
-            var menuAplicaciones = document.getElementById('menu-aplicaciones');
-            console.info(menuAplicaciones)
-            if(menuAplicaciones.className.includes("menu_is_active")){
-                menuAplicaciones.classList.remove("menu_is_active")
-            }else{
-                menuAplicaciones.classList.add("menu_is_active")
-            }
-        }
+            
+            $scope.app_large = (CONF.APP.toLowerCase()).trim() + "-header";
 
-        $scope.togglenotify = function() {
-            console.log("toogleNotifications");
-            var notificaciones = document.getElementById('notificaciones');
-            console.info(notificaciones);
-            if(notificaciones.className.includes("menu_is_active")){
-                notificaciones.classList.remove("menu_is_active")
-            }else{
-                notificaciones.classList.add("menu_is_active")
-            }
-        }
+            if (token_service.live_token()) {
+                $scope.isLogin = true
 
-        $scope.sidebarEvent= function(){
-            console.log("data");
-            var sidebarDiv = document.getElementById('menu-sidebar');
-            var sidebarContainer = document.getElementById('menu-sidebar-container');
-            var containerDiv= document.getElementById('container-div');   
-            var containerBody= document.getElementById('container-body-id');   
-            var containerLogoCollapsed= document.getElementById($scope.app); 
-            var containerLogo= document.getElementById($scope.app_large); 
-            console.log("container logo :"+containerLogo)
-            if (sidebarDiv.className.includes("sidebar_off")){
-                sidebarContainer.classList.add('main-container-sidebar')
-                sidebarContainer.classList.remove('main-container-sidebar-off')
-                sidebarDiv.classList.add('sidebar_is_active')
-                sidebarDiv.classList.remove('sidebar_off')
-                containerBody.classList.add('container-body-off')
-                containerBody.classList.remove('container-body')
-                containerLogo.style.display = "inline-block";
-                containerLogoCollapsed.style.display = "none";
-                //*********************/
-                containerDiv.classList.add('container-view')
-                containerDiv.classList.remove('container-view-sidebar-off')
-            }else{
-                containerBody.classList.add('container-body')
-                containerBody.classList.remove('container-body-off')
-                sidebarContainer.classList.add('main-container-sidebar-off')
-                sidebarContainer.classList.remove('main-container-sidebar')
-                sidebarDiv.classList.add('sidebar_off')
-                sidebarDiv.classList.remove('sidebar_is_active')
-                //*****************/
-                containerLogo.classList.remove('isotipo')
-                containerLogo.classList.add('header-logo-collapsed')
-                containerLogo.style.display = "none";
-                containerLogoCollapsed.style.display = "inline-block";
-                
+                $scope.token = token_service.getPayload();
+            }
+            $scope.logout = function () {
+                token_service.logout();
             }
 
-        }
-    });
+            $("body")
+                .mouseup(function (event) {
+                    console.log(event);
+                })
+
+
+            $scope.toogleCerrarSesion = function () {
+                var buttonCerrarSesion = document.getElementById('header-button-cerrarsesion-container');
+                if (buttonCerrarSesion.style.display === "none" || buttonCerrarSesion.style.display === "") {
+                    buttonCerrarSesion.style.display = "block";
+                } else {
+                    buttonCerrarSesion.style.display = "none";
+                }
+
+            }
+            $scope.toogleAplicaciones = function () {
+                var menuAplicaciones = document.getElementById('menu-aplicaciones');
+                console.info(menuAplicaciones)
+                if (menuAplicaciones.className.includes("menu_is_active")) {
+                    menuAplicaciones.classList.remove("menu_is_active")
+                } else {
+                    menuAplicaciones.classList.add("menu_is_active")
+                }
+            }
+
+            $scope.togglenotify = function () {
+                console.log("toogleNotifications");
+                var notificaciones = document.getElementById('notificaciones');
+                console.info(notificaciones);
+                if (notificaciones.className.includes("menu_is_active")) {
+                    notificaciones.classList.remove("menu_is_active")
+                } else {
+                    notificaciones.classList.add("menu_is_active")
+                }
+            }
+
+            $scope.sidebarEvent = function () {
+                console.log("data");
+                var sidebarDiv = document.getElementById('menu-sidebar');
+                var sidebarContainer = document.getElementById('menu-sidebar-container');
+                var containerDiv = document.getElementById('container-div');
+                var containerBody = document.getElementById('container-body-id');
+                var containerLogoCollapsed = document.getElementById($scope.app);
+                var containerLogo = document.getElementById($scope.app_large);
+                var textoMenuLateral = document.getElementsByClassName("menulateral-text");
+                if (sidebarDiv.className.includes("sidebar_off")) {
+                    for(var i =0, il = textoMenuLateral.length;i<il;i++){
+                        textoMenuLateral[i].classList.remove("oculto");
+                    }
+                    sidebarContainer.classList.add('main-container-sidebar')
+                    sidebarContainer.classList.remove('main-container-sidebar-off')
+                    sidebarDiv.classList.add('sidebar_is_active')
+                    sidebarDiv.classList.remove('sidebar_off')
+                    containerBody.classList.add('container-body-off')
+                    containerBody.classList.remove('container-body')
+                    containerLogo.style.display = "inline-block";
+                    
+                    containerLogoCollapsed.style.display = "none";
+                    //*********************/
+                    containerDiv.classList.add('container-view')
+                    containerDiv.classList.remove('container-view-sidebar-off')
+                } else {
+                    for(var i =0, il = textoMenuLateral.length;i<il;i++){
+                        textoMenuLateral[i].classList.add("oculto");
+                    }
+                    containerBody.classList.add('container-body')
+                    containerBody.classList.remove('container-body-off')
+                    sidebarContainer.classList.add('main-container-sidebar-off')
+                    sidebarContainer.classList.remove('main-container-sidebar')
+                    sidebarDiv.classList.add('sidebar_off')
+                    sidebarDiv.classList.remove('sidebar_is_active')
+                    //*****************/
+                    containerLogo.classList.remove('isotipo')
+                    containerLogo.classList.add('header-logo-collapsed')
+                    containerLogo.style.display = "none";
+                    containerLogoCollapsed.style.display = "inline-block";
+
+                }
+
+            }
+        });
+
+
--- app/core/menu-lateral/opciones-component/opciones-directive.js
+++ app/core/menu-lateral/opciones-component/opciones-directive.js
@@ -24,6 +24,8 @@ angular.module('core')
                 $scope.nivel = null;
                 $scope.tema = (CONF.CATEGORIA.toLowerCase()).trim();
                 $scope.paleta = CONF.THEMES;
+                $scope.app = (CONF.APP.toLowerCase()).trim() + "-isotipo";
+                $scope.app_large = (CONF.APP.toLowerCase()).trim() + "-header";
                 $scope.opciones = null;
                 $scope.redirect_url = function (path) {
                     var path_sub = path.substring(0, 4);
@@ -37,6 +39,31 @@ angular.module('core')
                     }
                 };
                 $scope.toogle = function (nivel, $event) {
+
+                    var sidebarDiv = document.getElementById('menu-sidebar');
+                    var sidebarContainer = document.getElementById('menu-sidebar-container');
+                    var containerDiv = document.getElementById('container-div');
+                    var containerBody = document.getElementById('container-body-id');
+                    var containerLogoCollapsed = document.getElementById($scope.app);
+                    var containerLogo = document.getElementById($scope.app_large);
+                    var textoMenuLateral = document.getElementsByClassName("menulateral-text");
+                    if (sidebarDiv.className.includes("sidebar_off")) {
+                        for(var i =0, il = textoMenuLateral.length;i<il;i++){
+                            textoMenuLateral[i].classList.remove("oculto");
+                        }
+                        sidebarContainer.classList.add('main-container-sidebar')
+                        sidebarContainer.classList.remove('main-container-sidebar-off')
+                        sidebarDiv.classList.add('sidebar_is_active')
+                        sidebarDiv.classList.remove('sidebar_off')
+                        containerBody.classList.add('container-body-off')
+                        containerBody.classList.remove('container-body')
+                        containerLogo.style.display = "inline-block";
+                        
+                        containerLogoCollapsed.style.display = "none";
+                        //*********************/
+                        containerDiv.classList.add('container-view')
+                        containerDiv.classList.remove('container-view-sidebar-off')
+                    }
                     console.info($event)
                     self.open = !self.open;
                     $scope.nivel = nivel
@@ -44,7 +71,7 @@ angular.module('core')
                     console.info($scope.opciones);
                     if (self.open) {
                         $scope.nivel.clase = 'content-menu-off';
-                        $scope.nivel.style_icon = 'opcion-left';
+                        $scope.nivel.style_icon = 'opcion-up';
                         console.log($scope.opciones);
                     } else {
                         $scope.nivel.clase = 'content-menu';
