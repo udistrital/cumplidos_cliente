e39dcc5259d39310381d5f85d867655a561aa9f5
Diego Gutierrez <diagutierrezro@unal.edu.co>
2021-01-07 17:22:55 -0600

    fix: Cambio en variables de entorno para test

--- app/scripts/environment/environment_test.js
+++ app/scripts/environment/environment_test.js
@@ -14,12 +14,12 @@ angular.module("contractualClienteApp").constant("CONF", {
   APP: "cumplidos", // Nombre de la app, esto cargará el logo.
   APP_MENU: "contratistas", // Ingrese valor de la aplicación asociado al menú registrado en wso2
   GENERAL: {
-    WSO2_SERVICE: "http://jbpm.udistritaloas.edu.co:8280/services",
+    WSO2_SERVICE: "https://jbpm.udistritaloas.edu.co:8280/services",
     ADMINISTRATIVA_MID_SERVICE:
       "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_mid_api/v1/",
     ADMINISTRATIVA_SERVICE:
       "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_crud_api/v1/",
-    CUMPLIDOS_CRUD_SERVICE:
+      CUMPLIDOS_CRUD_SERVICE:
       "http://pruebasapi.intranetoas.udistrital.edu.co:8511/v1/",
     CUMPLIDOS_MID_SERVICE:
       "http://pruebasapi2.intranetoas.udistrital.edu.co:8513/v1/",
@@ -36,7 +36,8 @@ angular.module("contractualClienteApp").constant("CONF", {
     FINANCIERA_SERVICE:
       "https://autenticacion.portaloas.udistrital.edu.co/apioas/financiera_crud_api/v1/",
     MODELS_SERVICE: "scripts/models/",
-    NOTIFICACION_WS: "ws://pruebasapi.portaloas.udistrital.edu.co:8116/ws/join",
+    NOTIFICACION_WS:
+      "wss://pruebasapi.portaloas.udistrital.edu.co:8116/ws/join",
     OIKOS_SERVICE:
       "https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v1/",
     CONTRATO_SERVICE:
