5c299639b7e3f3874680b8e52ca71eb96bfec9ab
stevencol <steven2088@hotmail.es>
2024-05-07 11:03:26 -0500

    "feat: se crea plantilla html y controlador historico_cumplidos udistrital/Cumplidos_y_certificaciones_documentacion#7"

---
+++ app/scripts/controllers/seguimientoycontrol/tecnico/historico_cumplidos.js
@@ -0,0 +1,11 @@
+"use strict";
+
+angular
+  .module("contractualClienteApp")
+  .controller("HistoricoPagosCtrl", function () {
+    self = this;
+
+    self.alerta = function () {
+      alert("Hola mundo");
+    };
+  });
