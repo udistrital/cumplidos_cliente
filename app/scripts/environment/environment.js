41f22672ab8d2dab2287ed20bdba53fefdd82e64
stevencol <steven2088@hotmail.es>
2024-09-09 11:56:53 -0500

    fix: Restautrar envs Se restaura archivo de envs , al estado anterior udistrital/Cumplidos_y_certificaciones_documentacion#139

--- app/scripts/environment/environment.js
+++ app/scripts/environment/environment.js
@@ -22,8 +22,7 @@ angular.module("contractualClienteApp").constant("CONF", {
     CUMPLIDOS_CRUD_SERVICE:
       "https://autenticacion.portaloas.udistrital.edu.co/apioas/cumplidos_crud/v1/",
     CUMPLIDOS_MID_SERVICE:
-     /// "https://autenticacion.portaloas.udistrital.edu.co/apioas/cumplidos_mid/v2/",
-     "http://localhost:8090/v1/",
+      "https://autenticacion.portaloas.udistrital.edu.co/apioas/cumplidos_mid/v2/",
     ADMINISTRATIVA_JBPM_V2:
       "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_jbpm/v2/",
     ADMINISTRATIVA_PRUEBAS_SERVICE:
@@ -34,38 +33,25 @@ angular.module("contractualClienteApp").constant("CONF", {
       "https://autenticacion.portaloas.udistrital.edu.co/apioas/core_api/v1/",
     DOCUMENTO_CRUD_SERVICE:
       "https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v2/",
-    FINANCIERA_MID_SERVICE:
-      "https://autenticacion.portaloas.udistrital.edu.co/apioas/financiera_mid_api/v1/",
-    FINANCIERA_SERVICE:
-      "https://autenticacion.portaloas.udistrital.edu.co/apioas/financiera_crud_api/v1/",
-    MODELS_SERVICE: "scripts/models/",
     NOTIFICACION_MID_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/notificacion_mid/v1/",
-    ARM_AWS_NOTIFICACIONES:"arn:aws:sns:us-east-1:699001025740:test-Cumplidos",
-    OIKOS_SERVICE:
-      "https://autenticacion.portaloas.udistrital.edu.co/apioas/oikos_crud_api/v1/",
-    CONTRATO_SERVICE:
-      "https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_jbpm/v1/",
-    FIRMA_ELECTRONICA_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/firma_electronica_mid/v1/",
-    FINANCIERA_JBPM_SERVICE:"https://autenticacion.portaloas.udistrital.edu.co/apioas/financiera_jbpm/v1/",
-    NUXEO_SERVICE: "https://documental.portaloas.udistrital.edu.co/nuxeo/",
-    NUXEO_MID:
-      "https://autenticacion.portaloas.udistrital.edu.co/apioas/nuxeo_mid/v1/",
+    ARM_AWS_NOTIFICACIONES:"arn:aws:sns:us-east-1:699001025740:production-Cumplidos",
     GESTION_DOCUMENTAL_SERVICE:"https://autenticacion.portaloas.udistrital.edu.co/apioas/gestor_documental_mid/v1",
-    TITAN_MID_SERVICE:'https://autenticacion.portaloas.udistrital.edu.co/apioas/titan_api_mid/v2/',
     AUTENTICATION_MID_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/",
+    FIRMA_ELECTRONICA_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/firma_electronica_mid/v1/",
+    MODELS_SERVICE: "scripts/models/",
     TOKEN: {
       AUTORIZATION_URL:
         "https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize",
       URL_USER_INFO:
         "https://autenticacion.portaloas.udistrital.edu.co/oauth2/userinfo",
-      CLIENTE_ID: "sWe9_P_C76DWGOsLcOY4T7BYH6oa",
-      REDIRECT_URL: "http://localhost:9000/",
+      CLIENTE_ID: "YM3WYl4Q0EujfkNkqeBlH7KvIpsa",
+      REDIRECT_URL: "https://contratistas.portaloas.udistrital.edu.co",
       RESPONSE_TYPE: "id_token token",
       SCOPE: "openid email documento",
       BUTTON_CLASS: "btn btn-custom btn-sm",
       SIGN_OUT_URL:
         "https://autenticacion.portaloas.udistrital.edu.co/oidc/logout",
-      SIGN_OUT_REDIRECT_URL: "http://localhost:9000/",
+      SIGN_OUT_REDIRECT_URL: "https://contratistas.portaloas.udistrital.edu.co",
       SIGN_OUT_APPEND_TOKEN: "true"
     }
   }
