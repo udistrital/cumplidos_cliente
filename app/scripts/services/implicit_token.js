0ce4938877d1deb668d42103c17fadc13f67a767
cristhianLagunaM <claguna201@gmail.com>
2025-08-25 15:19:04 -0500

    refactor: enhance token handling and local storage management

--- app/scripts/services/implicit_token.js
+++ app/scripts/services/implicit_token.js
@@ -1,247 +1,222 @@
 'use strict';
 
-if (!window.localStorage.getItem('access_token')) {
-  var params = {}, queryString = location.hash.substring(1), regex = /([^&=]+)=([^&]*)/g, m;
+/**
+ * @ngdoc overview
+ * @name implicitToken
+ * @description
+ * # implicitToken
+ * Service in the implicitToken.
+ */
+// First, parse the query string
+if (window.localStorage.getItem('access_token') === null ||
+  window.localStorage.getItem('access_token') === undefined) {
+  var params = {},
+    queryString = location.hash.substring(1),
+    regex = /([^&=]+)=([^&]*)/g;
+  var m;
   while ((m = regex.exec(queryString)) !== null) {
     params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
   }
-  if (params.id_token) {
+  // And send the token over to the server
+  var req = new XMLHttpRequest();
+  // consider using POST so query isn't logged
+  var query = 'https://' + window.location.host + '?' + queryString;
+  //
+  req.open('GET', query, true);
+  if (params.id_token !== null && params.id_token !== undefined) {
     var expires_at = new Date();
+    //console.log('id_token inicial: ',params.id_token)
     window.localStorage.setItem('access_token', params.access_token);
     window.localStorage.setItem('id_token', params.id_token);
     window.localStorage.setItem('state', params.state);
     window.localStorage.setItem('expires_in', params.expires_in);
-    expires_at.setSeconds(expires_at.getSeconds() + parseInt(window.localStorage.getItem('expires_in')) - 40);
+    expires_at.setSeconds(expires_at.getSeconds() + parseInt(window.localStorage.getItem('expires_in')) - 40); // 40 seconds less to secure browser and response latency
     window.localStorage.setItem('expires_at', expires_at);
   } else {
     window.localStorage.clear();
   }
+  req.onreadystatechange = function(e) {
+    if (req.readyState === 4) {
+      if (req.status === 200) {
+        //
+      } else if (req.status === 400) {
+
+      } else {
+
+      }
+    }
+  };
 }
 
 angular.module('implicitToken', [])
-  .factory('token_service', function ($q, CONF, md5, $interval, autenticacionMidRequest, $window, $rootScope) {
-    var mem = {};
-    var store = (function () {
-      try { sessionStorage.setItem('__t','1'); sessionStorage.removeItem('__t'); return sessionStorage; }
-      catch(e){ return { getItem:function(k){return mem[k]||null;}, setItem:function(k,v){mem[k]=v;}, removeItem:function(k){delete mem[k];} }; }
-    })();
-
-    var inFlightUserRol = null;
-    var loginDataPromise = null;
-
-    function tokenFingerprint() {
-      var at = $window.localStorage.getItem('access_token') || '';
-      return md5.createHash(at);
-    }
-
-    function rolesCacheValid() {
-      var fp = store.getItem('roles_for_token');
-      var code = store.getItem('access_code');
-      var role = store.getItem('access_role');
-      return !!fp && fp === tokenFingerprint() && !!code && !!role;
-    }
-
-    function saveRolesToCache(doc, role) {
-      store.setItem('access_code', btoa(JSON.stringify(doc)));
-      store.setItem('access_role', btoa(JSON.stringify(role)));
-      store.setItem('roles_for_token', tokenFingerprint());
-    }
-
-    function clearRolesCache() {
-      store.removeItem('access_code');
-      store.removeItem('access_role');
-      store.removeItem('roles_for_token');
-    }
-
-    function loginOnce() {
-      if (store.getItem('auth_redirecting') === '1') return;
-      store.setItem('auth_redirecting', '1');
-      service.login();
-    }
+  .factory('token_service', function ($q,CONF, md5, $interval,autenticacionMidRequest) {
 
     var service = {
+      //session: $localStorage.default(params),
       header: null,
       token: null,
       logout_url: null,
-      loaded_data: false,
-
-      getLoginData: function(autoRedirect) {
-        autoRedirect = !!autoRedirect;
-        if (loginDataPromise) return loginDataPromise;
-
+      loaded_data:false,
+      getLoginData: function() {
+        //Para  llamar el api de autenticacion
         var deferred = $q.defer();
-        loginDataPromise = deferred.promise;
+        if (window.localStorage.getItem('access_token') !== null &&
+          window.localStorage.getItem('access_token') !== undefined) {
+            //console.log('access_code existe',window.localStorage.getItem('access_code'))
+          if (window.localStorage.getItem('access_code') === null ||
+            window.localStorage.getItem('access_code') === undefined) {
+            var appUserInfo = JSON.parse(atob(window.localStorage.getItem('id_token').split('.')[1]));
+            var appUserDocument;
+            var appUserRole;
+            var userRol= {
+              user: appUserInfo.email
+            };
+            //console.log('entro ',appUserInfo)
+            //console.log((appUserInfo.role===null ||appUserInfo.role===undefined) &&(appUserInfo.documento===null ||appUserInfo.documento===undefined) && (appUserInfo.email!=null && appUserInfo.email!=undefined))
+            if((appUserInfo.role===null ||appUserInfo.role===undefined) &&(appUserInfo.documento===null ||appUserInfo.documento===undefined) && (appUserInfo.email!=null && appUserInfo.email!=undefined)){
+              autenticacionMidRequest.post("token/userRol", userRol, {
+                headers: {
+                  'Accept': 'application/json',
+                  "Authorization": "Bearer " + window.localStorage.getItem('access_token'),
+                }
+              })
+              .then(function(respuestaAutenticacion) {
+                appUserDocument = respuestaAutenticacion.data.documento;
 
-        if (!$window.localStorage.getItem('access_token')) {
-          if (autoRedirect) loginOnce();
-          deferred.resolve(true); loginDataPromise = null;
-          return deferred.promise;
-        }
-
-        var raw = $window.localStorage.getItem('id_token');
-        if (!raw) {
-          if (autoRedirect) loginOnce();
-          deferred.resolve(true); loginDataPromise = null;
-          return deferred.promise;
-        }
-
-        var appUserInfo = {};
-        try { appUserInfo = JSON.parse(atob(raw.split('.')[1])); }
-        catch (e) {
-          if (autoRedirect) loginOnce();
-          deferred.resolve(true); loginDataPromise = null;
-          return deferred.promise;
-        }
-
-        if (rolesCacheValid()) {
-          deferred.resolve(true); loginDataPromise = null;
-          return deferred.promise;
-        }
-
-        var needsUserRol = (
-          (appUserInfo.role === null || appUserInfo.role === undefined) &&
-          (appUserInfo.documento === null || appUserInfo.documento === undefined) &&
-          (appUserInfo.email != null && appUserInfo.email != undefined)
-        );
-
-        if (needsUserRol) {
-          if (!inFlightUserRol) {
-            inFlightUserRol = autenticacionMidRequest.post('token/userRol', { user: appUserInfo.email }, {
-              headers: {
-                'Accept': 'application/json',
-                'Authorization': 'Bearer ' + $window.localStorage.getItem('access_token'),
-              }
-            })
-            .then(function (resp) {
-              saveRolesToCache(resp.data.documento, resp.data.role);
-              service.setExpiresAt && service.setExpiresAt();
-              service.timer && service.timer();
-              $rootScope.$broadcast('auth:rolesLoaded', resp.data);
+                appUserRole = respuestaAutenticacion.data.role;
+                //console.log('respuesta autenticacion',appUserDocument,appUserRole)
+                window.location.reload();
+                window.localStorage.setItem('access_code', btoa(JSON.stringify(appUserDocument)));
+                window.localStorage.setItem('access_role', btoa(JSON.stringify(appUserRole)));
+                setExpiresAt();
+                timer();
+                //
+                deferred.resolve(true);
+              })
+              .catch(function(excepcionAutenticacion) {
+                //console.log("fallo la autenticacion");
+                //service.logout();
+              });
+            }else{
               deferred.resolve(true);
-            })
-            .catch(function () { deferred.reject(false); })
-            .finally(function () {
-              inFlightUserRol = null;
-              loginDataPromise = null;
-            });
+            }
           } else {
-            inFlightUserRol.then(function(){ deferred.resolve(true); })
-                           .catch(function(){ deferred.reject(false); })
-                           .finally(function(){ loginDataPromise = null; });
+            deferred.resolve(true);
           }
         } else {
           deferred.resolve(true);
-          loginDataPromise = null;
         }
-
         return deferred.promise;
       },
-
       generateState: function () {
         var text = ((Date.now() + Math.random()) * Math.random()).toString().replace('.', '');
         return md5.createHash(text);
       },
-
-      setting_bearer: { headers: {} },
-
+      setting_bearer: {
+        headers: {}
+      },
       getHeader: function () {
         service.setting_bearer = {
           headers: {
             'Accept': 'application/json',
-            "Authorization": "Bearer " + $window.localStorage.getItem('access_token'),
+            "Authorization": "Bearer " + window.localStorage.getItem('access_token'),
             "Content-Type": "application/json",
           }
         };
         return service.setting_bearer;
       },
-
       login: function () {
-        if (!CONF.GENERAL.TOKEN.nonce) CONF.GENERAL.TOKEN.nonce = service.generateState();
-        if (!CONF.GENERAL.TOKEN.state) CONF.GENERAL.TOKEN.state = service.generateState();
-        var base = CONF.GENERAL.TOKEN.AUTORIZACION_URL || CONF.GENERAL.TOKEN.AUTORIZATION_URL;
-        var url = base + '?' +
+        if (!CONF.GENERAL.TOKEN.nonce) {
+          CONF.GENERAL.TOKEN.nonce = service.generateState();
+        }
+        if (!CONF.GENERAL.TOKEN.state) {
+          CONF.GENERAL.TOKEN.state = service.generateState();
+        }
+        var url = CONF.GENERAL.TOKEN.AUTORIZATION_URL + '?' +
           'client_id=' + encodeURIComponent(CONF.GENERAL.TOKEN.CLIENTE_ID) + '&' +
           'redirect_uri=' + encodeURIComponent(CONF.GENERAL.TOKEN.REDIRECT_URL) + '&' +
           'response_type=' + encodeURIComponent(CONF.GENERAL.TOKEN.RESPONSE_TYPE) + '&' +
           'scope=' + encodeURIComponent(CONF.GENERAL.TOKEN.SCOPE);
-        if (CONF.GENERAL.TOKEN.nonce) url += '&nonce=' + encodeURIComponent(CONF.GENERAL.TOKEN.nonce);
+        if (CONF.GENERAL.TOKEN.nonce) {
+          url += '&nonce=' + encodeURIComponent(CONF.GENERAL.TOKEN.nonce);
+        }
         url += '&state=' + encodeURIComponent(CONF.GENERAL.TOKEN.state);
-        $window.location = url;
+        window.location = url;
         return url;
       },
-
       live_token: function () {
-        if ($window.localStorage.getItem('id_token') === 'undefined' ||
-            !$window.localStorage.getItem('id_token') || service.logoutValid()) {
+        if (window.localStorage.getItem('id_token') === 'undefined' || window.localStorage.getItem('id_token') === null || service.logoutValid()) {
+          //service.login();
           return false;
         } else {
           service.setting_bearer = {
             headers: {
               'Accept': 'application/json',
-              "Authorization": "Bearer " + $window.localStorage.getItem('access_token'),
+              "Authorization": "Bearer " + window.localStorage.getItem('access_token'),
             }
           };
           service.logout_url = CONF.GENERAL.TOKEN.SIGN_OUT_URL;
-          service.logout_url += '?id_token_hint=' + $window.localStorage.getItem('id_token');
+          service.logout_url += '?id_token_hint=' + window.localStorage.getItem('id_token');
           service.logout_url += '&post_logout_redirect_uri=' + CONF.GENERAL.TOKEN.SIGN_OUT_REDIRECT_URL;
-          service.logout_url += '&state=' + $window.localStorage.getItem('state');
+          service.logout_url += '&state=' + window.localStorage.getItem('state');
           return true;
         }
       },
-
       getPayload: function () {
-        var id_token = $window.localStorage.getItem('id_token').split('.');
+        var id_token = window.localStorage.getItem('id_token').split('.');
         return JSON.parse(atob(id_token[1]));
       },
-
       getAppPayload: function() {
-        var id_token = $window.localStorage.getItem('id_token').split('.');
-        var access_code = store.getItem('access_code');
-        var access_role = store.getItem('access_role');
+
+        var id_token = window.localStorage.getItem('id_token').split('.');
+        var access_code = window.localStorage.getItem('access_code');
+        var access_role = window.localStorage.getItem('access_role');
         var data = angular.fromJson(atob(id_token[1]));
-        if (!data.documento && access_code) data.documento = angular.fromJson(atob(access_code));
-        if (!data.role && access_role) data.role = angular.fromJson(atob(access_role));
+        //console.log('access_code:',access_code,'access_role: ',access_role);
+        if(!data.documento){
+          data.documento = angular.fromJson(atob(access_code));
+        }
+        if(!data.role){
+          data.role = angular.fromJson(atob(access_role));
+        }
+        //console.log('data:',data)
         return data;
       },
-
       logout: function () {
-        store.removeItem('auth_redirecting');
-        clearRolesCache();
-        store.removeItem('expires_at');
-        $window.localStorage.clear();
-        $window.location.replace("/");
+        window.localStorage.clear();
+        window.location.replace("/");
+        //console.log(service.logout_url)
+        // se elimina el redirect
+        //window.location.replace(service.logout_url);
       },
-
       expired: function () {
-        var ea = store.getItem('expires_at');
-        return (ea && new Date(ea) < new Date());
+        return ( window.localStorage.getItem('expires_at')!==null && new Date(window.localStorage.getItem('expires_at')) < new Date());
       },
 
       setExpiresAt: function () {
-        if (!store.getItem('expires_at')) {
+        if (angular.isUndefined(window.localStorage.getItem('expires_at')) || window.localStorage.getItem('expires_at') === null) {
           var expires_at = new Date();
-          var ei = parseInt($window.localStorage.getItem('expires_in')) || 3600;
-          expires_at.setSeconds(expires_at.getSeconds() + ei - 40);
-          store.setItem('expires_at', expires_at);
+          expires_at.setSeconds(expires_at.getSeconds() + parseInt(window.localStorage.getItem('expires_in')) - 40); // 40 seconds less to secure browser and response latency
+          window.localStorage.setItem('expires_at', expires_at);
         }
       },
 
       timer: function () {
-        if (store.getItem('expires_at') && store.getItem('expires_at') !== 'Invalid Date') {
-          var mostrado = false;
+        if (!angular.isUndefined(window.localStorage.getItem('expires_at')) || window.localStorage.getItem('expires_at') === null || window.localStorage.getItem('expires_at') === 'Invalid Date') {
+          var mostrado=false
           $interval(function () {
-            var restante = new Date(); restante.setMinutes(restante.getMinutes() + 5);
-            var ea = store.getItem('expires_at');
-            if (ea && new Date(ea) < restante && !mostrado){
-              mostrado = true;
+            var restante = new Date()
+            restante.setMinutes(restante.getMinutes()+5)
+            if (window.localStorage.getItem('expires_at') !== null && new Date(window.localStorage.getItem('expires_at')) < restante && !mostrado){
+              mostrado=true
               swal({
-                title: 'Su sesión caducará pronto, por favor asegúrese de guardar todos los cambios',
+                title: 'Su sesión caducará pronto, por favor asegurese de guardar todos los cambios',
                 type: 'error',
                 showCancelButton: false,
                 confirmButtonColor: '#d33',
                 confirmButtonText: 'Aceptar'
-              });
-            } else if (service.expired()) {
+              })
+            }else if (service.expired()) {
               swal({
                 title: 'Su sesión ha caducado, por favor vuelva a ingresar',
                 type: 'error',
@@ -249,33 +224,41 @@ angular.module('implicitToken', [])
                 confirmButtonColor: '#d33',
                 confirmButtonText: 'Aceptar'
               }).then(function () {
-                loginOnce();
-              });
+              service.logout();
+              })
             }
           }, 5000);
+        } else {
+          window.location.reload();
         }
       },
 
       logoutValid: function () {
-        var state, valid, queryString = location.search.substring(1), regex = /([^&=]+)=([^&]*)/g, m;
-        while (!!(m = regex.exec(queryString))) { state = decodeURIComponent(m[2]); }
-        if ($window.localStorage.getItem('state') === state) {
-          service.clearStorage(); valid = true;
-        } else valid = false;
+        var state;
+        var valid;
+        var queryString = location.search.substring(1);
+        var regex = /([^&=]+)=([^&]*)/g;
+        var m;
+        while (!!(m = regex.exec(queryString))) {
+          state = decodeURIComponent(m[2]);
+        }
+        if (window.localStorage.getItem('state') === state) {
+          service.clearStorage();
+          valid = true;
+        } else {
+          valid = false;
+        }
         return valid;
       },
-
       clearStorage: function () {
-        clearRolesCache();
-        store.removeItem('expires_at');
-        $window.localStorage.removeItem('access_token');
-        $window.localStorage.removeItem('id_token');
-        $window.localStorage.removeItem('expires_in');
-        $window.localStorage.removeItem('state');
+        window.localStorage.removeItem('access_token');
+        window.localStorage.removeItem('id_token');
+        window.localStorage.removeItem('expires_in');
+        window.localStorage.removeItem('state');
+        window.localStorage.removeItem('expires_at');
       }
     };
-
+    //service.setExpiresAt();
     service.timer();
-    store.removeItem('auth_redirecting');
     return service;
   });
